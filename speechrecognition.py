eaf06adee7a905ec41e1b93c31cb74bdfd3d012c
kunjkinger <63980854+kunjkinger@users.noreply.github.com>
2021-07-20 01:00:43 +0100

    Delete speechrecognition.py

--- speechrecognition.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition
-
-recognizer = speech_recognition.Recognizer()
-with speech_recognition.Microphone() as source:
-    print("say something!")
-    audio = recognizer.listen(source)
-
-print("Google speech recognition thinks you said: ")
-print(recognizer.recognize_google(audio))
